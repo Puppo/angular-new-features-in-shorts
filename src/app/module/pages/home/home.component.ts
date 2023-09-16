72e21c0da75465ffb48c843b56f0dbcf1cc28cb4
Luca Del Puppo <luca@delpuppo.net>
2023-09-16 11:48:38 +0200

    destroyref

--- src/app/module/pages/home/home.component.ts
+++ src/app/module/pages/home/home.component.ts
@@ -1,14 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { DoSomethingService } from '../../services/do-something.service';
 
 @Component({
   selector: 'app-home',
-  template: `<p>Home with Module works!</p>`,
+  template: `<p>Home with Module works! {{ time | date : 'mediumTime' }}</p>`,
 })
-export class HomeComponent implements OnInit {
-  constructor(protected doSomethingSv: DoSomethingService) {}
+export class HomeComponent implements OnInit, OnDestroy {
+  protected intervalId: ReturnType<typeof setInterval>;
+  public time = new Date();
+
+  constructor(protected doSomethingSv: DoSomethingService) {
+    this.intervalId = setInterval(() => {
+      console.log('setInterval');
+      this.time = new Date();
+    }, 300);
+  }
 
   ngOnInit() {
     this.doSomethingSv.doSomething();
+  }
+
+  ngOnDestroy(): void {
+    console.log('HomeComponent destroyed');
+    clearInterval(this.intervalId);
   }
 }
--- src/app/standalone/pages/home/home.component.ts
+++ src/app/standalone/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, DestroyRef, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DoSomethingService } from '../../services/do-something.service';
 
@@ -6,10 +6,24 @@ import { DoSomethingService } from '../../services/do-something.service';
   selector: 'app-home',
   standalone: true,
   imports: [CommonModule],
-  template: '<p>Home with Standalone Components works!</p>',
+  template:
+    '<p>Home with Standalone Components works! {{ time | date : "mediumTime" }}</p>',
 })
 export class HomeComponent implements OnInit {
   protected doSomethingSv: DoSomethingService = inject(DoSomethingService);
+  protected destroyRef: DestroyRef = inject(DestroyRef);
+
+  public time = new Date();
+
+  constructor() {
+    const id = setInterval(() => {
+      console.log('setInterval');
+      this.time = new Date();
+    }, 300);
+    this.destroyRef.onDestroy(() => {
+      clearInterval(id);
+    });
+  }
 
   ngOnInit() {
     this.doSomethingSv.doSomething();
